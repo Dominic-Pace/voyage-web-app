92bed2f0858451e073e0f9a92675b8386b332d00
Dominic-Pace <dpace@nhpace.net>
2018-07-31 20:23:47 -0500

    Have navigation tabs become responsive when selected

--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -2,17 +2,25 @@ import React from 'react'
 
 import NavItem from './item'
 
+const homePath = '/'
+const explorePath = '/explore'
+
 const NavigationTabs = () => (
   <div className="nav-header float-lg-right">
     <div className="nav-container">
+      {console.log('window', window.location.pathname)}
       <NavItem
-        linkTo="/"
-        selected
+        linkTo={homePath}
+        selected={window.location.pathname === homePath}
       >
         Home
       </NavItem>
       <NavItem
-        linkTo="/explore"
+        linkTo={explorePath}
+        selected={
+          window.location.pathname.includes(explorePath) ||
+          window.location.pathname.includes('package')
+        }
       >
         Explore
       </NavItem>
