89bde3a37b34ab19cee7bbec3c36652d0369bf07
Dominic-Pace <dpace@nhpace.net>
2018-08-05 16:07:24 -0500

    Modified stepper connector bar color on completed steps.

--- src/core/booking/stepper/index.js
+++ src/core/booking/stepper/index.js
@@ -5,6 +5,7 @@ const BookingStepper = ({ currentStep, steps }) => (
   <Stepper
     activeColor="#F1557E"
     activeStep={currentStep}
+    completeBarColor="#005656"
     completeColor="#39BCBC"
     size={24}
     steps={steps}
