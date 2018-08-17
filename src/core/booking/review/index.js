93dc74ca09b93c78a3191812cebd303724053a79
Dominic-Pace <dpace@nhpace.net>
2018-08-16 23:08:47 -0500

    Begin booking review page.

--- src/core/booking/review/index.js
+++ src/core/booking/review/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
-import { Grid } from 'react-bootstrap'
+import { Col, Grid, Row } from 'react-bootstrap'
+import { H1 } from '../../../components/text'
 
 import '../styles.css'
 
-const ReviewView = ({ currentPackage, selectedAccommodation, selectedActivities }) => (
-  <Grid className="booking-view-container">
-    Review view
-  </Grid>
-)
+const ReviewView = ({ currentPackage, selectedAccommodation, selectedActivities }) => {
+  console.log('Current Package', currentPackage)
+  console.log('Selected Accommodations', selectedAccommodation)
+  console.log('Selected Activities', selectedActivities)
+  return (
+    <Grid className="booking-review-container">
+      <Row
+        className="booking-review-header-image"
+        style={{ backgroundImage: `url(${currentPackage.imageUrl})`}}
+      >
+        <H1
+          bol
+          center
+          color="#FFF"
+          rowClassName="booking-review-header-text-container"
+          style={{
+            textShadow: '1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000',
+          }}
+        >
+          Review Your Order
+        </H1>
+      </Row>
+    </Grid>
+  )
+}
 
 export default ReviewView
